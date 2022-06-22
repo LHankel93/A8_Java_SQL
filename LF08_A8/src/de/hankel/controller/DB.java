d0adda8ad533f886a48c20b8b6506c53bac811b0
LHankel93 <lorenz.hankel@yahoo.de>
2022-06-22 22:59:38 +0200

    Hauptansicht updated

--- LF08_A8/src/de/hankel/controller/DB.java
+++ LF08_A8/src/de/hankel/controller/DB.java
@@ -2,6 +2,7 @@ package de.hankel.controller;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -16,6 +17,9 @@ public class DB {
 	Statement stmt;
 	ResultSet rs;
 
+	/**
+	 * Standardkonstruktor für den DB Controller.
+	 */
 	public DB() {
 		driver = "com.mysql.jdbc.Driver";
 		url = "jdbc:mysql://localhost/egamedarling?";
@@ -29,19 +33,35 @@ public class DB {
 		}
 	}
 
+	/**
+	 * Diese Methode prüft, ob die Daten in der Account Tabelle vorhanden sind und stimmen.
+	 * Gibt true zurück wenn gut, false wenn schlecht.
+	 * @param email Die E-Mail auf welche geprüft werden soll.
+	 * @param passwort Das Passwort, welches für die E-Mail angelegt sein soll.
+	 * @return true wenn Eingaben korrekt, false wenn falsch.
+	 */
 	public Boolean checkLogin(String email, String passwort) {
 		try {
-			stmt = con.createStatement();
-			
+			// Projektion mit PreparedStatement vorbereiten
+			PreparedStatement ps = con.prepareStatement("SELECT email, passwort FROM t_accounts WHERE email =?");
+			ps.setString(1, email);
+			rs = ps.executeQuery();
+			// Durch ResultSet iterieren und nochmal prüfen, ob Daten korrekt
+			while (rs.next()) {
+				if (email.compareTo(rs.getString(1)) == 0 && passwort.compareTo(rs.getString(2)) == 0) {
+					return true;
+				}
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
-		return true;
+		return false;
 	}
 
+	/**
+	 * Methode zum prüfen der DB.
+	 */
 	public void testdb() {
-
 		try {
 			stmt = con.createStatement();
 			rs = stmt.executeQuery("SELECT * FROM t_accounts");
--- LF08_A8/src/de/hankel/view/Hauptansicht.java
+++ LF08_A8/src/de/hankel/view/Hauptansicht.java
@@ -1,17 +1,13 @@
 package de.hankel.view;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
 import de.hankel.controller.DB;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JTabbedPane;
 import java.awt.Color;
 import javax.swing.ImageIcon;
@@ -22,24 +18,26 @@ import java.awt.Font;
 import javax.swing.UIManager;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
+import javax.swing.SwingConstants;
 
 public class Hauptansicht extends JFrame {
-	
-	private DB db;
 
-	/**
-	 * 
-	 */
+	private DB db;
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JPasswordField passwordField;
 
 	/**
-	 * Create the frame.
+	 * Standardkonstruktor des Haupt-Fensters. Soll Login und Registrieren
+	 * ermöglichen.
+	 * 
+	 * @param db Ein Objekt der Controller Klasse DB. Sollte vorher initialisiert
+	 *           und deklariert worden sein.
 	 */
 	public Hauptansicht(DB db) {
 		setBackground(Color.WHITE);
-		setIconImage(Toolkit.getDefaultToolkit().getImage(Hauptansicht.class.getResource("/img/egamedarlingdb_logo_small.png")));
+		setIconImage(Toolkit.getDefaultToolkit()
+				.getImage(Hauptansicht.class.getResource("/img/egamedarlingdb_logo_small.png")));
 		setTitle("EgameDarling Login/Registrieren");
 		this.db = db;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -48,27 +46,29 @@ public class Hauptansicht extends JFrame {
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout(0, 0));		
-		
+		contentPane.setLayout(new BorderLayout(0, 0));
+
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		tabbedPane.setBackground(Color.WHITE);
 		contentPane.add(tabbedPane);
-		
+
 		JPanel panelLogin = new JPanel();
 		panelLogin.setBackground(Color.WHITE);
-		tabbedPane.addTab("Login", new ImageIcon(Hauptansicht.class.getResource("/img/arrow-right-to-bracket-solid_small.png")), panelLogin, "Hier können Sie sich in der EGameDarling Datenbank einloggen.");
+		tabbedPane.addTab("Login",
+				new ImageIcon(Hauptansicht.class.getResource("/img/arrow-right-to-bracket-solid_small.png")),
+				panelLogin, "Hier können Sie sich in der EGameDarling Datenbank einloggen.");
 		tabbedPane.setEnabledAt(0, true);
 		SpringLayout sl_panelLogin = new SpringLayout();
 		panelLogin.setLayout(sl_panelLogin);
-		
+
 		JTextArea textAreaEmail = new JTextArea();
 		textAreaEmail.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		textAreaEmail.setBackground(UIManager.getColor("Label.background"));
 		sl_panelLogin.putConstraint(SpringLayout.NORTH, textAreaEmail, 10, SpringLayout.NORTH, panelLogin);
 		sl_panelLogin.putConstraint(SpringLayout.EAST, textAreaEmail, -10, SpringLayout.EAST, panelLogin);
 		panelLogin.add(textAreaEmail);
-		
+
 		JLabel lblEmail = new JLabel("E-Mail");
 		sl_panelLogin.putConstraint(SpringLayout.WEST, textAreaEmail, 40, SpringLayout.EAST, lblEmail);
 		lblEmail.setIcon(new ImageIcon(Hauptansicht.class.getResource("/img/at-solid_small.png")));
@@ -77,16 +77,67 @@ public class Hauptansicht extends JFrame {
 		lblEmail.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		sl_panelLogin.putConstraint(SpringLayout.WEST, lblEmail, 10, SpringLayout.WEST, panelLogin);
 		panelLogin.add(lblEmail);
-		
+
 		JLabel lblPasswort = new JLabel("Passwort");
 		lblPasswort.setIcon(new ImageIcon(Hauptansicht.class.getResource("/img/key-solid_small.png")));
 		lblPasswort.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		sl_panelLogin.putConstraint(SpringLayout.NORTH, lblPasswort, 6, SpringLayout.SOUTH, lblEmail);
 		sl_panelLogin.putConstraint(SpringLayout.WEST, lblPasswort, 0, SpringLayout.WEST, lblEmail);
 		panelLogin.add(lblPasswort);
+
+		passwordField = new JPasswordField();
+		passwordField.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		sl_panelLogin.putConstraint(SpringLayout.NORTH, passwordField, 0, SpringLayout.NORTH, lblPasswort);
+		sl_panelLogin.putConstraint(SpringLayout.WEST, passwordField, 0, SpringLayout.WEST, textAreaEmail);
+		sl_panelLogin.putConstraint(SpringLayout.SOUTH, passwordField, 0, SpringLayout.SOUTH, lblPasswort);
+		sl_panelLogin.putConstraint(SpringLayout.EAST, passwordField, 0, SpringLayout.EAST, textAreaEmail);
+		panelLogin.add(passwordField);
+
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setActionCommand("loginValidieren");
+
+		sl_panelLogin.putConstraint(SpringLayout.NORTH, btnLogin, 30, SpringLayout.SOUTH, lblPasswort);
+		btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		sl_panelLogin.putConstraint(SpringLayout.WEST, btnLogin, 0, SpringLayout.WEST, lblEmail);
+		panelLogin.add(btnLogin);
+
+		JButton btnRegistrieren = new JButton("Registrieren");
+		btnRegistrieren.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnRegistrieren.setActionCommand("geheZuTabRegistrieren");
+
+		sl_panelLogin.putConstraint(SpringLayout.NORTH, btnRegistrieren, 0, SpringLayout.NORTH, btnLogin);
+		sl_panelLogin.putConstraint(SpringLayout.WEST, btnRegistrieren, 10, SpringLayout.EAST, btnLogin);
+		sl_panelLogin.putConstraint(SpringLayout.SOUTH, btnRegistrieren, 0, SpringLayout.SOUTH, btnLogin);
+		panelLogin.add(btnRegistrieren);
+
+		JPanel panelRegistrieren = new JPanel();
+		panelRegistrieren.setBackground(Color.WHITE);
+		tabbedPane.addTab("Registrieren",
+				new ImageIcon(Hauptansicht.class.getResource("/img/user-plus-solid_small.png")), panelRegistrieren,
+				"Hier kann ein neuer Benutzer-Account in der EGameDarling Datenbank angelegt werden.");
+		tabbedPane.setBackgroundAt(1, Color.WHITE);
+		panelRegistrieren.setLayout(new SpringLayout());
+
+		JPanel panelErfolg = new JPanel();
+		panelErfolg.setBackground(Color.WHITE);
+		panelErfolg.setVisible(false);
+		tabbedPane.addTab("Erfolgreicher Login", null, panelErfolg, null);
+		SpringLayout sl_panelErfolg = new SpringLayout();
+		panelErfolg.setLayout(sl_panelErfolg);
 		
-		
-		
+		JLabel lblLoginErfolgreich = new JLabel("LOGIN ERFOLGREICH!");
+		lblLoginErfolgreich.setBackground(Color.GREEN);
+		sl_panelErfolg.putConstraint(SpringLayout.SOUTH, lblLoginErfolgreich, -10, SpringLayout.SOUTH, panelErfolg);
+		sl_panelErfolg.putConstraint(SpringLayout.EAST, lblLoginErfolgreich, -10, SpringLayout.EAST, panelErfolg);
+		lblLoginErfolgreich.setHorizontalAlignment(SwingConstants.CENTER);
+		sl_panelErfolg.putConstraint(SpringLayout.NORTH, lblLoginErfolgreich, 10, SpringLayout.NORTH, panelErfolg);
+		sl_panelErfolg.putConstraint(SpringLayout.WEST, lblLoginErfolgreich, 10, SpringLayout.WEST, panelErfolg);
+		lblLoginErfolgreich.setFont(new Font("Tahoma", Font.PLAIN, 38));
+		lblLoginErfolgreich.setOpaque(true);
+		lblLoginErfolgreich.setBackground(new Color(0.15f, 1.0f, 0.25f, 0.3333f));
+		panelErfolg.add(lblLoginErfolgreich);
+		tabbedPane.setEnabledAt(2, false);
+
 		// Action Listener implementieren für Action Commands
 		// Action Listener implementieren
 		ActionListener aL = new ActionListener() {
@@ -100,42 +151,28 @@ public class Hauptansicht extends JFrame {
 					break;
 				}
 				case "loginValidieren": {
-
-					
+					// Hack um aus char[] ein String zu formen
+					String pw = "";
+					for (char c : passwordField.getPassword()) {
+						pw += c;
+					}
+					// Prüfen des Logins durch Controller
+					if (db.checkLogin(textAreaEmail.getText(), pw)) {
+//						System.out.println("Gut.");
+						panelErfolg.setVisible(true);
+						panelErfolg.setEnabled(true);
+						tabbedPane.setSelectedIndex(2);
+					} else {
+//						System.out.println("Schlecht.");
+					}
 					break;
 				}
 				}
 			}
 		};
-		
-		passwordField = new JPasswordField();
-		passwordField.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		sl_panelLogin.putConstraint(SpringLayout.NORTH, passwordField, 0, SpringLayout.NORTH, lblPasswort);
-		sl_panelLogin.putConstraint(SpringLayout.WEST, passwordField, 0, SpringLayout.WEST, textAreaEmail);
-		sl_panelLogin.putConstraint(SpringLayout.SOUTH, passwordField, 0, SpringLayout.SOUTH, lblPasswort);
-		sl_panelLogin.putConstraint(SpringLayout.EAST, passwordField, 0, SpringLayout.EAST, textAreaEmail);
-		panelLogin.add(passwordField);
-		
-		JButton btnLogin = new JButton("Login");
-		btnLogin.setEnabled(false);
-		sl_panelLogin.putConstraint(SpringLayout.NORTH, btnLogin, 30, SpringLayout.SOUTH, lblPasswort);
-		btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		sl_panelLogin.putConstraint(SpringLayout.WEST, btnLogin, 0, SpringLayout.WEST, lblEmail);
-		panelLogin.add(btnLogin);
-		
-		JButton btnRegistrieren = new JButton("Registrieren");
-		btnRegistrieren.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnRegistrieren.setActionCommand("geheZuTabRegistrieren");
+
+		// Actionlistener hinzufügen
+		btnLogin.addActionListener(aL);
 		btnRegistrieren.addActionListener(aL);
-		sl_panelLogin.putConstraint(SpringLayout.NORTH, btnRegistrieren, 0, SpringLayout.NORTH, btnLogin);
-		sl_panelLogin.putConstraint(SpringLayout.WEST, btnRegistrieren, 10, SpringLayout.EAST, btnLogin);
-		sl_panelLogin.putConstraint(SpringLayout.SOUTH, btnRegistrieren, 0, SpringLayout.SOUTH, btnLogin);
-		panelLogin.add(btnRegistrieren);
-		
-		JPanel panelRegistrieren = new JPanel();
-		panelRegistrieren.setBackground(Color.WHITE);
-		tabbedPane.addTab("Registrieren", new ImageIcon(Hauptansicht.class.getResource("/img/user-plus-solid_small.png")), panelRegistrieren, "Hier kann ein neuer Benutzer-Account in der EGameDarling Datenbank angelegt werden.");
-		tabbedPane.setBackgroundAt(1, Color.WHITE);
-		panelRegistrieren.setLayout(new SpringLayout());
 	}
 }
